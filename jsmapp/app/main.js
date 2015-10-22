07bce38ffc0257e8fc12c560ae33dee333c745a3
Anna Blackwell <annablackwell44@gmail.com>
2015-10-23 09:46:59 +1300

    Update main.js

--- jsmapp/app/main.js
+++ jsmapp/app/main.js
@@ -1,4 +1,4 @@
-import R from 'ramda'
+import { range, reduce, reverse, subtract, sortBy } from 'ramda'
 
 var numbers = [7, 12, 4, 51, 23]
 
@@ -6,6 +6,16 @@ var add = function (x, y) {
   return x + y
 }
 
-var sum = R.reduce(add, 0, numbers)
+var sum = reduce(add, 0, numbers)
+
+/* var subtract = function (a, b) {
+  return a - b
+}
+
+var minus = reduce(subtract, a, b) */
 
 console.log('sum =', sum)
+
+console.log('range =', range(5, 15).toString())
+
+/* console.log('subtract =' minus(8, 3)) */
